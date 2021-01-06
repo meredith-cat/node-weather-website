f02231ca5d132509d1626a9bce802eac23713f0a
Meredith Gallop <meredith@trtl.co.uk>
2021-01-06 16:01:14 +0000

    added humidity to weather display

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (lattitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=18d1e4870bbb648fae4401c5f79087f8&query='+lattitude+','+longitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=18d1e4870bbb648fae4401c5f79087f8&query='+lattitude+','+longitude
 
     request({url, json: true}, (error, {body}) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (lattitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+ ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+            callback(undefined, "The current weather is: " + body.current.weather_descriptions[0]+". It is " + body.current.temperature + "°C. It feels like " + body.current.feelslike + "°C. The humidity is "+body.current.humidity+".")
                 } 
     })
 }
